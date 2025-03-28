e6d4cf2b6bbc81c87e4f4cbfbe6badc72235342d
romin014 <sohyun014@naver.com>
2025-03-28 18:50:22 +0900

    comment : Commit message test

--- src/main/java/com/example/boot_board/BootBoardApplication.java
+++ src/main/java/com/example/boot_board/BootBoardApplication.java
@@ -2,7 +2,9 @@ package com.example.boot_board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BootBoardApplication {
 
---
+++ src/main/java/com/example/boot_board/entity/Board.java
@@ -0,0 +1,21 @@
+package com.example.boot_board.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "board")
+public class Board extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "longtext")
+    private String contents;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
